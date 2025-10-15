bfe39642ce54365e281ef31e3d3a5da842afc3ed
Bui Anh Tuan <tuan.ai.research@gmail.com>
2025-10-15 15:16:10 +0700

    feat: initial project setup with Vite, React, TS and Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-slate-900 min-h-screen flex justify-center items-center">
+      <h1 className="text-white text-4xl font-bold underline">
+        Hello World! It's working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
