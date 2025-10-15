bb5d1a8baa8afb45cdfc086120ec7ac663e12859
Bui Anh Tuan <tuan.ai.research@gmail.com>
2025-10-15 15:29:13 +0700

    feat: integrate Vercel Speed Insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { SpeedInsights } from "@vercel/speed-insights/react"
+
 function App() {
   return (
+    <>
     <div className="bg-slate-900 min-h-screen flex justify-center items-center">
       <h1 className="text-white text-4xl font-bold underline">
         Hello World! It's working!
       </h1>
     </div>
+    <SpeedInsights /> 
+    </>
   )
 }
 
